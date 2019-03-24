c2187d91453d74cd25ad6b74281537ead48c965d
ryverine <rwcheek@gmail.com>
2019-03-24 08:10:39 -0400

    Created file structure. Added skeleton HTML to index. Added keyUp event and input validation to javascript.

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+var words = []; //array to hold all words player will guess
+
+// find # of char in longest word, make array that size
+var spanElements = [];
+
+//the key that the user pressed
+var userInput = "";
+
+//starts game
+document.onkeyup = function(event) 
+{
+    userInput = event.key.toUpperCase();
+    //get user input, only want A-Z
+    alert("userInput: " + userInput + "\n" +
+        "A-Z? -> " + userInput.match(/[a-z]/i)
+        );
+}
+
+function isValidInput(theInput)
+{
+    theInput = theInput.toUpperCase();
+
+    var valid = false;
+
+    var acceptedInput = [
+        "A","B","C","D","E","F","G","H","I",
+        "J","K","L","M","N","O","P","Q","R",
+        "S","T","U","V","W","X","Y","Z"];
+
+    for (var i = 0; i < acceptedInput.length; i++)
+    {
+        if(theInput === acceptedInput[i])
+        {
+            valid = true;
+            break;
+        }
+    }
+
+    if(valid === true)
+    {
+        console.log("User entered: " + theInput);
+    }
+    else
+    {
+        console.log("User entered invalid character: " + theInput);
+    }
+
+    return valid;
+}
+ 
+
