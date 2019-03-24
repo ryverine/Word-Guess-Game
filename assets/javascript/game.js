e08e05acc483b47801ad89aeb25c2cfc36b8a912
ryverine <rwcheek@gmail.com>
2019-03-24 17:48:01 -0400

    Building function that will fill array with words in random order.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,36 @@
-var words = []; //array to hold all words player will guess
+
+// number of times word was found before guesses run out
+var wins = 0;
+
+// number of attempts player has to find word
+// guessCount needs to at least match word length
+var guessCount = 0;
+
+// all the letters the player has tried
+// ignore a guess if letter is already in lettersUsed
+var lettersUsed = "";
+
+// the array of words
+// make function to load array in random order
+var words = []; // array to hold all words player will guess
 
 // find # of char in longest word, make array that size
-var spanElements = [];
+var characterElements = [];
 
-//the key that the user pressed
+// the key that the user pressed
 var userInput = "";
 
-//starts game
+// starts game
 document.onkeyup = function(event) 
 {
     userInput = event.key.toUpperCase();
-    //get user input, only want A-Z
+    // get user input, only want A-Z
     var validInput = isValidInput(userInput);
 }
 
 function isValidInput(theInput)
 {
+    console.log("*** isValidInput("+theInput+") ***");
     theInput = theInput.toUpperCase();
 
     var valid = false;
@@ -44,6 +59,43 @@ function isValidInput(theInput)
     }
 
     return valid;
+}
+
+// might need to scrap this:
+function buildWordArray()
+{
+    console.log("*** buildWordArray() ***");
+    // pre-defined values
+    var wordsToUse = [];
+
+    // must equal same length as wordsTo Use
+    var resultArray = [];
+
+    var arrayIndex = 0;
+
+    while(wordsToUse.length != resultArray.length)
+    {
+        console.log("wordsToUse[" + i + "]: " +  wordsToUse[i]);
+
+        // get random number, between 0 and length
+        var rng = Math.floor(Math.random() * Math.floor(wordsToUse.length));
+        
+        // get the word at that index of wordsTouse
+        // is that word already in resultArray?
+        // array.indexOf(item, start)
+        // if so: break and run loop again
+        // if not: resultArray.push(wordsToUse[random#])
+        // arrayIndex++;
+
+
+
+        
+                // need to look at this math more...
+
+    }
+
+    //return wordsToUse
+
 }
  
 
